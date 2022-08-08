7f891e0a5ec64afa10703362a8ab175b75038854
zhongwei.hua <huazhongwei@yeah.net>
2022-08-08 18:04:06 +0800

    1、我的

--- app/src/main/java/com/hzw/wan/ui/mine/MineScene.kt
+++ app/src/main/java/com/hzw/wan/ui/mine/MineScene.kt
@@ -1,10 +1,81 @@
 package com.hzw.wan.ui.mine
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.hzw.wan.R
+import com.zj.banner.utils.ImageLoader
 
 @Composable
 fun MineScreen(navController: NavController) {
-    Text(text = "Mine")
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primary)
+                .fillMaxWidth()
+                .height(180.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_default),
+                    contentDescription = "header",
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(70.dp, 70.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "未登录",
+                    style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.onPrimary),
+                    modifier = Modifier.padding(top = 10.dp)
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .padding(start = 10.dp)
+                .clickable { },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_palette),
+                contentDescription = "theme"
+            )
+            Text(
+                text = "主题",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(start = 10.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.ic_arrow_right),
+                contentDescription = "arrowRight",
+                modifier = Modifier.wrapContentWidth(align = Alignment.End)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .fillMaxWidth()
+                .background(color = Color.Gray)
+                .height(1.dp)
+        )
+    }
+
 }
